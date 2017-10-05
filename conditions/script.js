f1416ce0ab6d29d7fbbcb9d5133d018438b8707b
Luka Nedeljkovic <westsidenbgd@gmail.com>
2017-10-05 17:53:28 +0200

    initial commit

---
+++ conditions/script.js
@@ -0,0 +1,19 @@
+var num1 = 11;
+ if((typeof num1) === "number") {    
+console.log("This is number");
+
+    if(num1 % 2 === 0) {
+
+        var result= num1/ 2;
+        console.log(result);
+        console.log(num1 + " /2 = " + result);
+
+    }else {
+
+        console.log ("x");
+
+
+       }
+
+
+ }
